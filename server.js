af92595a8ef3f304be64dadd972c3206117d664f
Xianyi555 <xianyi555@gmail.com>
2017-12-20 00:10:51 -0800

    made html file and tested localhost 3000

--- server.js
+++ server.js
@@ -11,3 +11,25 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.get('/', function homepage(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is up and running on http://localhost:3000/');
+});
