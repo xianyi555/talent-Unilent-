381443d6957927b6d3f71b29c0d9f8b58774602b
Xianyi555 <xianyi555@gmail.com>
2017-12-20 10:50:21 -0800

    added usersList

---
+++ seed.js
@@ -0,0 +1,23 @@
+var db = require("./models");
+
+var usersList = [{
+  name: 'John',
+  email: 'johntest@gmail.com',
+  location: 'San Francisco',
+  talents: [ 'soft-engineer', 'writing' ]
+}, {
+  name: 'Lily',
+  email: 'lilytest@gmail.com',
+  location: 'New York',
+  talents: [ 'ice-skating' ]
+}, {
+  name: 'Jack',
+  email: 'jacktest@gmail.com',
+  location: 'Chicago',
+  talents: [ 'teaching', 'singing', 'dacing', 'swimming' ]
+}, {
+  name: 'Ann',
+  email: 'anntest@gmail.com',
+  location: 'Boston',
+  talents: [ 'piano', 'painting' ]
+}];
