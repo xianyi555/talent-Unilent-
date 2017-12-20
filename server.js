8d608a2c7465c728990987969a5cfbb9c4144d0a
Xianyi555 <xianyi555@gmail.com>
2017-12-19 23:54:57 -0800

    required bodyParser, allowed cross origin requests

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 var express = require('express'),
     app = express();
 
-    
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// allow cross origin requests (optional)
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
