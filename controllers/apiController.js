85608d0ecde72858cacf83bb1d848553cd5fb3e2
Xianyi555 <xianyi555@gmail.com>
2017-12-20 01:44:33 -0800

    added endpoints in apiController, and corrected heroku baseUrl

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,7 +2,7 @@ function index(req, res) {
   res.json({
     message: 'Welcome to Talent-Network!',
     documentation_url: 'https://github.com/sf-wdi-labs/talent-network',
-    baseUrl: "https://mysterious-everglades-48142.herokuapp.com", 
+    baseUrl: "https://fierce-atoll-30421.herokuapp.com", 
     base_url: 'localhost:3000',
     endpoints: [
       {
